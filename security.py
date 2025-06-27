ee8e4e093ecb1bdc34012d301d1fd0891cf0ccd4
Flourish Olukotun <flourisholukotun@gmail.com>
2025-06-27 18:50:23 +0100

    Added Cors Middleware in security.py

--- security.py
+++ security.py
@@ -1,12 +1,24 @@
 import secrets
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.middleware.cors import CORSMiddleware
+from main import app
 
 load_dotenv()
 
 security = HTTPBasic()
+
+# CORS Middleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 USERNAME = os.getenv("BASIC_AUTH_USERNAME")
 PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
